41019befb6ddd9bdc53897d0f91a5d893ebbe313
jithware <jithware@gmail.com>
2022-09-10 17:00:40 -0400

    Update demo

--- integration_test/demo_test.dart
+++ integration_test/demo_test.dart
@@ -1,5 +1,7 @@
-// To execute test run:
+// To execute demo run:
 // flutter test integration_test/demo_test.dart
+// To execute demo with screenshots saved run:
+// flutter drive --no-pub --driver=integration_test/driver.dart --target=integration_test/demo_test.dart
 
 import 'package:brethap/constants.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +10,7 @@ import 'package:integration_test/integration_test.dart';
 
 import 'package:brethap_wear/main.dart';
 import 'package:brethap_wear/main.dart' as app;
+import 'screenshot.dart';
 
 // ignore_for_file: dead_code
 bool demoRunning = true, demoPresets = true, demoCustom = true;
@@ -15,7 +18,7 @@ bool demoRunning = true, demoPresets = true, demoCustom = true;
 const Duration wait = Duration(milliseconds: 1000);
 
 Future<void> main() async {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Demo', (WidgetTester tester) async {
@@ -26,7 +29,9 @@ Future<void> main() async {
     String envVars = "";
 
     await tester.pump(wait);
-    envVars += "HOME_SNAP=${stopwatch.elapsed - wait}\n";
+
+    await tester.pumpAndSettle();
+    takeScreenshot(binding, "1_home.png");
 
     // Running
     if (demoRunning) {
@@ -39,10 +44,11 @@ Future<void> main() async {
       await tester.tap(finder);
       await tester.pump(wait);
 
-      envVars += "INHALE_SNAP=${stopwatch.elapsed + wait * 2}\n";
-
       // running
       for (int i = 0; i < 100; i++) {
+        if (i == 20) {
+          takeScreenshot(binding, "2_inhale.png");
+        }
         await tester.pump(const Duration(milliseconds: 100));
       }
 
@@ -52,7 +58,7 @@ Future<void> main() async {
       await tester.tap(finder);
 
       await tester.pumpAndSettle();
-      envVars += "DURATION_SNAP=${stopwatch.elapsed}\n";
+      takeScreenshot(binding, "3_duration.png");
 
       // Tapping title closes snackbar
       await tester.pump(wait);
@@ -72,7 +78,7 @@ Future<void> main() async {
       await tester.tap(find.byType(PopupMenuButton<String>));
 
       await tester.pumpAndSettle();
-      envVars += "PRESET1_SNAP=${stopwatch.elapsed}\n";
+      takeScreenshot(binding, "4_preset.png");
 
       await tester.pump(wait);
 
@@ -81,7 +87,7 @@ Future<void> main() async {
           find.byKey(const Key(DEFAULT_TEXT)), const Offset(0, -1));
 
       await tester.pumpAndSettle();
-      envVars += "PRESET2_SNAP=${stopwatch.elapsed}\n";
+      takeScreenshot(binding, "5_preset.png");
 
       // tap default
       await tester.pump(wait);
@@ -105,7 +111,7 @@ Future<void> main() async {
       await tester.tap(find.byKey(const Key(HomeWidget.phonePreference)));
 
       await tester.pumpAndSettle();
-      envVars += "CUSTOM_SNAP=${stopwatch.elapsed}\n";
+      takeScreenshot(binding, "6_custom.png");
 
       await tester.pump(wait);
 
@@ -120,6 +126,9 @@ Future<void> main() async {
       await tester.pumpAndSettle();
       envVars += "CUSTOM_END=${stopwatch.elapsed}\n";
     }
+
+    await tester.pumpAndSettle();
+    takeScreenshot(binding, "7_dark.png");
 
     await tester.pump(wait);
     envVars += "DEMO_END=${stopwatch.elapsed}\n";
